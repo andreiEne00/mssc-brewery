c53fa00edebb03ab59acd47063f2845b700ade19
andreiEne00 <andrei.ene@softvision.com>
2021-12-08 17:16:55 +0200

    [MAJOR] Addded POST, PUT, DELETE for BeerController.java and CustomerController.java

--- src/main/java/andreiene/springproj/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/andreiene/springproj/msscbrewery/services/CustomerServiceImpl.java
@@ -1,10 +1,12 @@
 package andreiene.springproj.msscbrewery.services;
 
 import andreiene.springproj.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
     @Override
@@ -12,5 +14,23 @@ public class CustomerServiceImpl implements CustomerService {
         return CustomerDto.builder().id(UUID.randomUUID())
                 .name("Andrei Ene")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .name("George Tarnauceanu")
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        //todo implement
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        log.debug("Deleting a customer..");
     }
 }
--- src/main/java/andreiene/springproj/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/andreiene/springproj/msscbrewery/web/controller/CustomerController.java
@@ -3,6 +3,7 @@ package andreiene.springproj.msscbrewery.web.controller;
 import andreiene.springproj.msscbrewery.services.CustomerService;
 import andreiene.springproj.msscbrewery.web.model.BeerDto;
 import andreiene.springproj.msscbrewery.web.model.CustomerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,9 +21,33 @@ public class CustomerController {
     }
 
 
-    @GetMapping("/{customerId}")
+    @GetMapping({"/{customerId}"})
     public ResponseEntity<CustomerDto> getCustomer(@PathVariable("customerId") UUID customerId) {
         return new ResponseEntity<>(customerService.getCustomerById(customerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody CustomerDto customerDto) {
+        CustomerDto savedDto = customerService.saveNewCustomer(customerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/customer/" + savedDto.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping({"/customerId"})
+    public ResponseEntity handleUpdate(@PathVariable("customerId") UUID customerId, @RequestBody CustomerDto customerDto) {
+
+        customerService.updateCustomer(customerId, customerDto);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping({"/customerId"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCustomer(@PathVariable("customerId") UUID customerId) {
+        customerService.deleteById(customerId);
     }
 
 }
