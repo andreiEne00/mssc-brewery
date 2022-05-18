8b9d98493d09c946839ccd2972bc4aa2fd076a4c
andreiEne00 <andrei.ene@softvision.com>
2022-05-18 23:07:53 +0300

    [MAJOR] MVC Validation - Added validation closes

--- src/main/java/andreiene/springproj/msscbrewery/web/model/BeerDto.java
+++ src/main/java/andreiene/springproj/msscbrewery/web/model/BeerDto.java
@@ -1,13 +1,17 @@
 package andreiene.springproj.msscbrewery.web.model;
 
+import andreiene.springproj.msscbrewery.web.model.v2.BeerStyleEnum;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Data
@@ -19,12 +23,29 @@ public class BeerDto {
     @Null
     private UUID id;
 
+    @Null
+    private Integer version;
+
+    @Null
+    private OffsetDateTime createdDate;
+
+    @Null
+    private OffsetDateTime lastModifiedDate;
+
     @NotBlank
     private String beerName;
 
-    @NotBlank
-    private String beerStyle;
+    @NotNull
+    private BeerStyleEnum beerStyle;
 
+    @NotNull
     @Positive
     private Long upc;
+
+    @Positive
+    @NotNull
+    BigDecimal price;
+
+
+    private Integer quantityOnHand;
 }
